ce009f58eed07469d6ff8c6eb4edadb42c3c36c3
Artur Silva <artursoaares@hotmail.com>
2023-07-06 12:00:13 -0300

    adicionando tudo

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Calc , Buttons, Input, StyledButtons } from './style';
+import CustomNumberInput from './style';
+
+
 
 function App() {
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Calc>
+        <CustomNumberInput value={value} onChange={handleChange} />
+        <Buttons>
+          {/* Numbers */}
+          <StyledButtons>
+            <button>ON/CE</button>
+            <button></button>
+            <button></button>
+            <button></button>
+          </StyledButtons>
+          <StyledButtons>
+            <button>off</button>
+            <button>&radic;</button>
+            <button>&#37;</button>
+            <button>&divide;</button>
+          </StyledButtons>
+          <StyledButtons>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>&times;</button>
+          </StyledButtons>
+          <StyledButtons>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>&minus;</button>
+          </StyledButtons>
+          <StyledButtons>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>&#43;</button>
+          </StyledButtons>
+          <StyledButtons>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+            <button>&#43;</button>
+          </StyledButtons>
+        </Buttons>
+        
+      </Calc>
+    </Container>
   );
 }
 
